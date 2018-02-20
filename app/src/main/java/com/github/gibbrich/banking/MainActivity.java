82d7a4d05620d1906941b914799d83d934368149
Arthur Dvurechenskiy <gibbrich@YANDEX.RU>
2018-02-20 22:47:59 +0300

    Update

--- app/src/main/java/com/github/gibbrich/banking/MainActivity.java
+++ app/src/main/java/com/github/gibbrich/banking/MainActivity.java
@@ -1,10 +1,15 @@
 package com.github.gibbrich.banking;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.github.gibbrich.banking.databinding.ActivityMainBinding;
+import com.github.gibbrich.banking.model.AccountType;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,5 +21,6 @@ public class MainActivity extends AppCompatActivity
 		
 		ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 		binding.setViewModel(new ViewModel());
+		binding.setSpinnerAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, AccountType.values()));
 	}
 }
--- app/src/main/java/com/github/gibbrich/banking/ViewModel.kt
+++ app/src/main/java/com/github/gibbrich/banking/ViewModel.kt
@@ -1,12 +1,17 @@
 package com.github.gibbrich.banking
 
+
+import com.github.gibbrich.banking.model.AccountType
+
 /**
  * Created by Dvurechenskiyi on 20.02.2018.
  */
 class ViewModel
 {
-    fun isVisible() : Boolean
+    var accountType: AccountType = AccountType.ACCOUNT
+
+    fun isMFOFieldVisible(): Boolean
     {
-        return false
+        return accountType == AccountType.ACCOUNT
     }
 }
