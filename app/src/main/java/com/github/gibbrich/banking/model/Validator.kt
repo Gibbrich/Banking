aadaa36aafa3282f23ecdf58172d554b24ae189d
Arthur Dvurechenskiyi <Dvurechenskiyi@cdc.ru>
2018-02-21 16:17:40 +0300

    Update

---
+++ app/src/main/java/com/github/gibbrich/banking/model/Validator.kt
@@ -0,0 +1,8 @@
+package com.github.gibbrich.banking.model
+
+/**
+ * Created by Dvurechenskiyi on 21.02.2018.
+ */
+abstract class Validator()
+
+class PredicateValidator(open val predicate: Predicate)
